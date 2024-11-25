d6d34c59cbcac2bb454b15ed61fbaa0fc7fc08fb
manojprajapti928 <manojprajapti928@gmail.com>
2024-11-25 15:57:26 +0530

    login and signup api

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('EAuction', 'root', 'Manoj@123', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-});
-
-module.exports = sequelize;
