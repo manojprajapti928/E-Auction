983cc407120d92b189853778be36e92a2b959ada
Aaisha Khan <160912968+Aaisha011@users.noreply.github.com>
2024-11-25 22:20:00 +0530

    files are updated

--- frontend/src/Component/Home.jsx
+++ frontend/src/Component/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import img from "../Image/car.webp";
 import { useNavigate } from "react-router";
 import Details from './Details'
+import Navbar from "../Components/Navbar";
 
 export default function Home() {
   const Navigate = useNavigate();
@@ -24,144 +25,147 @@ export default function Home() {
   };
 
   return (
-    <div className="m-1 flex flex-row flex-wrap justify-center items-center">
-      <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
-        <div>
-          <img src={img} alt="Car" className="rounded-md mb-2" />
+    <>
+    <Navbar/>
+      <div className="m-1 flex flex-row flex-wrap justify-center items-center">
+        <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
+          <div>
+            <img src={img} alt="Car" className="rounded-md mb-2" />
+          </div>
+          <div>
+            <p className="text-lg font-bold">Name: Car</p>
+            <p className="text-md">Number of Bids: 3</p>
+            <p className="text-lg font-bold">Price: $1000</p>
+            <p className="text-red-700 font-bold mt-2">
+              Time Left: {formatTime(timeLeft)}
+            </p>
+            <button
+              onClick={() => Navigate("/Details")}
+              className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700"
+            >
+              Click here
+            </button>
+          </div>
         </div>
-        <div>
-          <p className="text-lg font-bold">Name: Car</p>
-          <p className="text-md">Number of Bids: 3</p>
-          <p className="text-lg font-bold">Price: $1000</p>
-          <p className="text-red-700 font-bold mt-2">
-            Time Left: {formatTime(timeLeft)}
-          </p>
-          <button
-            onClick={() => Navigate("/Details")}
-            className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700"
-          >
-            Click here
-          </button>
-        </div>
-      </div>
 
-      <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
-        <div>
-          <img src={img} alt="Car" className="rounded-md mb-2" />
+        <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
+          <div>
+            <img src={img} alt="Car" className="rounded-md mb-2" />
+          </div>
+          <div>
+            <p className="text-lg font-bold">Name: Car</p>
+            <p className="text-md">Number of Bids: 3</p>
+            <p className="text-lg font-bold">Price: $1000</p>
+            <p className="text-red-700 font-bold mt-2">
+              Time Left: {formatTime(timeLeft)}
+            </p>
+            <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
+              Click here
+            </button>
+          </div>
         </div>
-        <div>
-          <p className="text-lg font-bold">Name: Car</p>
-          <p className="text-md">Number of Bids: 3</p>
-          <p className="text-lg font-bold">Price: $1000</p>
-          <p className="text-red-700 font-bold mt-2">
-            Time Left: {formatTime(timeLeft)}
-          </p>
-          <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
-            Click here
-          </button>
-        </div>
-      </div>
 
-      <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
-        <div>
-          <img src={img} alt="Car" className="rounded-md mb-2" />
+        <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
+          <div>
+            <img src={img} alt="Car" className="rounded-md mb-2" />
+          </div>
+          <div>
+            <p className="text-lg font-bold">Name: Car</p>
+            <p className="text-md">Number of Bids: 3</p>
+            <p className="text-lg font-bold">Price: $1000</p>
+            <p className="text-red-700 font-bold mt-2">
+              Time Left: {formatTime(timeLeft)}
+            </p>
+            <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
+              Click here
+            </button>
+          </div>
         </div>
-        <div>
-          <p className="text-lg font-bold">Name: Car</p>
-          <p className="text-md">Number of Bids: 3</p>
-          <p className="text-lg font-bold">Price: $1000</p>
-          <p className="text-red-700 font-bold mt-2">
-            Time Left: {formatTime(timeLeft)}
-          </p>
-          <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
-            Click here
-          </button>
-        </div>
-      </div>
 
-      <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
-        <div>
-          <img src={img} alt="Car" className="rounded-md mb-2" />
+        <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
+          <div>
+            <img src={img} alt="Car" className="rounded-md mb-2" />
+          </div>
+          <div>
+            <p className="text-lg font-bold">Name: Car</p>
+            <p className="text-md">Number of Bids: 3</p>
+            <p className="text-lg font-bold">Price: $1000</p>
+            <p className="text-red-700 font-bold mt-2">
+              Time Left: {formatTime(timeLeft)}
+            </p>
+            <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
+              Click here
+            </button>
+          </div>
         </div>
-        <div>
-          <p className="text-lg font-bold">Name: Car</p>
-          <p className="text-md">Number of Bids: 3</p>
-          <p className="text-lg font-bold">Price: $1000</p>
-          <p className="text-red-700 font-bold mt-2">
-            Time Left: {formatTime(timeLeft)}
-          </p>
-          <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
-            Click here
-          </button>
-        </div>
-      </div>
 
-      <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
-        <div>
-          <img src={img} alt="Car" className="rounded-md mb-2" />
+        <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
+          <div>
+            <img src={img} alt="Car" className="rounded-md mb-2" />
+          </div>
+          <div>
+            <p className="text-lg font-bold">Name: Car</p>
+            <p className="text-md">Number of Bids: 3</p>
+            <p className="text-lg font-bold">Price: $1000</p>
+            <p className="text-red-700 font-bold mt-2">
+              Time Left: {formatTime(timeLeft)}
+            </p>
+            <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
+              Click here
+            </button>
+          </div>
         </div>
-        <div>
-          <p className="text-lg font-bold">Name: Car</p>
-          <p className="text-md">Number of Bids: 3</p>
-          <p className="text-lg font-bold">Price: $1000</p>
-          <p className="text-red-700 font-bold mt-2">
-            Time Left: {formatTime(timeLeft)}
-          </p>
-          <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
-            Click here
-          </button>
+        <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
+          <div>
+            <img src={img} alt="Car" className="rounded-md mb-2" />
+          </div>
+          <div>
+            <p className="text-lg font-bold">Name: Car</p>
+            <p className="text-md">Number of Bids: 3</p>
+            <p className="text-lg font-bold">Price: $1000</p>
+            <p className="text-red-700 font-bold mt-2">
+              Time Left: {formatTime(timeLeft)}
+            </p>
+            <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
+              Click here
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
-        <div>
-          <img src={img} alt="Car" className="rounded-md mb-2" />
-        </div>
-        <div>
-          <p className="text-lg font-bold">Name: Car</p>
-          <p className="text-md">Number of Bids: 3</p>
-          <p className="text-lg font-bold">Price: $1000</p>
-          <p className="text-red-700 font-bold mt-2">
-            Time Left: {formatTime(timeLeft)}
-          </p>
-          <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
-            Click here
-          </button>
-        </div>
-      </div>
 
-      <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
-        <div>
-          <img src={img} alt="Car" className="rounded-md mb-2" />
+        <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
+          <div>
+            <img src={img} alt="Car" className="rounded-md mb-2" />
+          </div>
+          <div>
+            <p className="text-lg font-bold">Name: Car</p>
+            <p className="text-md">Number of Bids: 3</p>
+            <p className="text-lg font-bold">Price: $1000</p>
+            <p className="text-red-700 font-bold mt-2">
+              Time Left: {formatTime(timeLeft)}
+            </p>
+            <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
+              Click here
+            </button>
+          </div>
         </div>
-        <div>
-          <p className="text-lg font-bold">Name: Car</p>
-          <p className="text-md">Number of Bids: 3</p>
-          <p className="text-lg font-bold">Price: $1000</p>
-          <p className="text-red-700 font-bold mt-2">
-            Time Left: {formatTime(timeLeft)}
-          </p>
-          <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
-            Click here
-          </button>
-        </div>
-      </div>
 
-      <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
-        <div>
-          <img src={img} alt="Car" className="rounded-md mb-2" />
-        </div>
-        <div>
-          <p className="text-lg font-bold">Name: Car</p>
-          <p className="text-md">Number of Bids: 3</p>
-          <p className="text-lg font-bold">Price: $1000</p>
-          <p className="text-red-700 font-bold mt-2">
-            Time Left: {formatTime(timeLeft)}
-          </p>
-          <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
-            Click here
-          </button>
+        <div className="w-[45vh] h-[57vh] bg-slate-300 border-2 border-black rounded-md p-3 shadow-lg shadow-black m-3">
+          <div>
+            <img src={img} alt="Car" className="rounded-md mb-2" />
+          </div>
+          <div>
+            <p className="text-lg font-bold">Name: Car</p>
+            <p className="text-md">Number of Bids: 3</p>
+            <p className="text-lg font-bold">Price: $1000</p>
+            <p className="text-red-700 font-bold mt-2">
+              Time Left: {formatTime(timeLeft)}
+            </p>
+            <button className="bg-blue-900 text-white h-[7vh] w-[13vw] cursor-pointer m-5 rounded-md ml-7 hover:bg-blue-700 hover:text-black active:bg-green-700">
+              Click here
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
